bf1dd3b22e250a26be91629154025fe272c1c38d
RAIHAN <raihanpplg11@gmail.com>
2024-08-29 20:43:28 +0700

    menambah kan view data

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <!-- Main Content -->
+    <main class="max-w-4xl mx-auto p-6 bg-white shadow-lg mt-6 rounded-lg">
+        <article>
+            <h2 class="text-2xl font-bold mb-4">{{ $post['title'] }}</h2>
+            <p class="text-gray-500 text-sm mb-4">Published on August 29, 2024 {{ $post['author'] }}</p>
+            <a href="/posts">&laquo; back to blog </a>
+            <img src="https://via.placeholder.com/800x400" alt="Tailwind CSS" class="w-full h-auto rounded-lg mb-6">
+
+            <p class="mb-4">
+                {{$post['body']}}
+            </p>
+        </article>
+    </main>
+
+
+</x-layout>
